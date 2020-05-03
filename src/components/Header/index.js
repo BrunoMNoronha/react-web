339ed2ad74e657bfcfe184495fe18083b91f1da9
Bruno M Noronha <bruno.developer@pm.me>
2020-05-03 20:10:00 -0300

    Aplicação WEB consumindo dados de uma api

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './styles.css';
+
+const Header = () => (
+    <header id="main-header">React WEB</header>
+);
+
+// Conceito de estado
+// class Header extends Component {
+//     render() {
+//         return (
+//             <h1></h1>
+//         );
+//     }
+// }
+
+export default Header;
